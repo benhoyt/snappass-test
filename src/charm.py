bb59867460de4e5716a5b7bf5e82748d68e9c62c
Ben Hoyt <benhoyt@gmail.com>
2021-02-19 17:48:33 +1300

    initial commit'

---
+++ src/charm.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# Copyright 2021 Ben
+# See LICENSE file for licensing details.
+
+"""Charm the service."""
+
+import logging
+import os
+import re
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.framework import StoredState
+from ops.model import ActiveStatus
+
+
+logger = logging.getLogger(__name__)
+
+
+LAYER_RE = re.compile(r'\d{3}-.+\.yaml')
+
+
+def add_layer(container_name, layer_yaml, layer_name='layer',
+              layers_dir_format='/charm/containers/{}/pebble/layers'):
+    layers_dir = layers_dir_format.format(container_name)
+    os.makedirs(layers_dir, exist_ok=True)
+
+    layers = sorted(n for n in os.listdir(layers_dir) if LAYER_RE.match(n))
+    if layers:
+        last = int(layers[-1].split('-', 1)[0])
+        assert 1 <= last <= 999, last
+    else:
+        last = 0
+    filename = os.path.join(layers_dir, '{:03}-{}.yaml'.format(last + 1, layer_name))
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(layer_yaml)
+
+    logger.info('wrote layer to {}'.format(filename))
+
+
+class SnappassTestCharm(CharmBase):
+    """Charm the service."""
+
+    _stored = StoredState()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.snappass_workload_ready, self._on_snappass_workload_ready)
+        self.framework.observe(self.on.redis_workload_ready, self._on_redis_workload_ready)
+        self._stored.set_default(things=[])
+
+    def _on_config_changed(self, _):
+        # Note: you need to uncomment the example in the config.yaml file for this to work (ensure
+        # to not just leave the example, but adapt to your configuration needs)
+        current = self.config["thing"]
+        if current not in self._stored.things:
+            logger.debug("found a new thing: %r", current)
+            self._stored.things.append(current)
+
+    def _on_snappass_workload_ready(self, event):
+        logger.info('_on_snappass_workload_ready')
+        add_layer('snappass', """
+summary: snappass layer
+description: snappass layer
+services:
+    snappass:
+        override: replace
+        summary: snappass service
+        command: snappass
+""", layer_name='snappass')
+        self.unit.containers['snappass'].start('snappass')
+        self.unit.status = ActiveStatus("snappass started")
+
+    def _on_redis_workload_ready(self, event):
+        logger.info('_on_redis_workload_ready')
+        add_layer('redis', """
+summary: redis layer
+description: redis layer
+services:
+    redis:
+        override: replace
+        summary: redis service
+        command: redis-server
+""", layer_name='redis')
+        self.unit.containers['redis'].start('redis')
+        self.unit.status = ActiveStatus("redis started")
+
+
+if __name__ == "__main__":
+    main(SnappassTestCharm)
